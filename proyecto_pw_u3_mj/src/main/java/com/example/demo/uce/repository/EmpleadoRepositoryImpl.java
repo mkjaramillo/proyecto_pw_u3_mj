5c6e1a885b66909e9da6bebc665a8795f4c5b664
mkjaramillo <85204686+mkjaramillo@users.noreply.github.com>
2022-08-17 19:49:26 -0500

    Add files via upload
    taller 24 spring tools

--- proyecto_pw_u3_mj/src/main/java/com/example/demo/uce/repository/EmpleadoRepositoryImpl.java
+++ proyecto_pw_u3_mj/src/main/java/com/example/demo/uce/repository/EmpleadoRepositoryImpl.java
@@ -1,5 +1,39 @@
 package com.example.demo.uce.repository;
 
-public class EmpleadoRepositoryImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.uce.repository.modelo.Empleado;
+@Repository
+@Transactional
+public class EmpleadoRepositoryImpl implements IEmpleadoRepository {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Override
+	public void crear(Empleado empleado) {
+		this.entityManager.persist(empleado);
+	}
+
+	@Override
+	public void actualizar(Empleado empleado) {
+		this.entityManager.merge(empleado);
+		
+	}
+	@Override
+	public Empleado buscar(Integer id) {
+		return 		this.entityManager.find(Empleado.class, id);
+
+	}
+	@Override
+	public void eliminar(Integer id) {
+		this.entityManager.remove(this.buscar(id));
+	}
+
+	
+	
 
 }
