a149f6ced0418af8419f810ae131457a2b862e75
mkjaramillo <85204686+mkjaramillo@users.noreply.github.com>
2022-08-21 18:37:05 -0500

    tarea16 estudiante controller

---
+++ proyecto_pw_u3_mj/src/main/java/com/example/demo/uce/controller/EstudianteRestFulController.java
@@ -0,0 +1,65 @@
+package com.example.demo.uce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.uce.repository.modelo.Empleado;
+import com.example.demo.uce.repository.modelo.Estudiante;
+import com.example.demo.uce.service.IEstudianteService;
+
+@RestController
+@RequestMapping("/APILista/V1/estudiante")
+public class EstudianteRestFulController {
+	@Autowired
+private IEstudianteService estudianteService;
+@PostMapping
+public String crearEstudiante(@RequestBody Estudiante estudiante) {
+	String mensaje = "Estudiante insertado con exito";
+	try {
+		this.estudianteService.crear(estudiante);
+	} catch (Exception e) {
+		mensaje = "Error intente mas tarde";
+
+	}
+
+	return mensaje;
+}
+@PutMapping
+public String actualizarEstudiante(@RequestBody Estudiante estudiante) {
+	String mensaje = "Empleado actualizado con exito";
+	try {
+		this.estudianteService.actualizar(estudiante);
+	} catch (Exception e) {
+		mensaje = "Error intente mas tarde";
+
+	}
+
+	return mensaje;
+}
+@GetMapping(path="/{idEstudiante}")
+public ResponseEntity<Estudiante>buscarEstudiante(@PathVariable("idEstudiante") Integer id){
+	Estudiante est=this.estudianteService.buscarId(id);
+	
+	return ResponseEntity.ok(est);
+}
+@DeleteMapping(path="/{idEstudiante}")
+public String eliminarEstudiante(@PathVariable("idEstudiante")Integer idEst) {
+	String mensaje = "Estudiante eliminado con exito";
+	try {
+		this.estudianteService.eliminar(idEst);
+	} catch (Exception e) {
+		mensaje = "Error intente mas tarde";
+
+	}
+
+	return mensaje;
+}
+}
